7f4273bc71ff29a1cfb358f413b0dc3cc26b40ef
KChoi0720 <choicanlight@gmail.com>
2025-03-06 23:23:53 +1300

    # Add github actions token

--- build/git_update.sh
+++ build/git_update.sh
@@ -14,10 +14,17 @@ NEW_TAG=$(echo "$CURRENT_TAG" | awk -F. -v type="$VERSION_TYPE" '{
   if (type == "major") { $1 += 1; $2 = 0; $3 = 0 } 
   else if (type == "minor") { $2 += 1; $3 = 0 } 
   else { $3 += 1 } 
-  print $1"."$2"."$3 
+  print "v"$1"."$2"."$3 
 }')
 
-# Create and push new tag
+# Authenticate using GitHub Actions token
+git config --global user.email "github-actions@github.com"
+git config --global user.name "github-actions"
+
+# Ensure the remote repository is correctly set up
+git remote set-url origin https://x-access-token:${{ secrets.GITHUB_TOKEN }}@github.com/${{ github.repository }}.git
+
+# Create and push the new tag
 git tag "$NEW_TAG"
 git push origin "$NEW_TAG"
 
