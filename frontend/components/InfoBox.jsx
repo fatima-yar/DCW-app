38612817c87ac7795ba5302eb46bf77e79243da1
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-03-31 15:54:12 +1300

    add image ourmission

---
+++ frontend/components/InfoBox.jsx
@@ -0,0 +1,11 @@
+import OurMissionPhoto from './ui/OurMissionPhoto'
+
+export default function InfoBox() {
+  return (
+    <>
+      <div className="mx-50 py-20 text-black bg-white">
+        <OurMissionPhoto />
+      </div>
+    </>
+  )
+}
