16e43fb6333ea1b646f6c6b4f3f57bcc66d71183
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-04-14 15:49:13 +1200

    update newsletter component

---
+++ frontend/components/Newsletter.jsx
@@ -0,0 +1,46 @@
+export default function Newsletter() {
+  return (
+    <>
+      <div
+        style={{
+          position: 'relative',
+          width: '100%',
+          height: 0,
+          paddingTop: '141.4286%',
+          paddingBottom: 0,
+          // boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)',
+          marginTop: '1.6em',
+          marginBottom: '0.9em',
+          overflow: 'hidden',
+          borderRadius: '8px',
+          willChange: 'transform',
+        }}
+      >
+        <iframe
+          loading="lazy"
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            left: 0,
+            border: 'none',
+            padding: 0,
+            margin: 0,
+          }}
+          src="https://www.canva.com/design/DAGeJvmyQXo/sQA9RwG84mUQQZ7w2yYVcQ/view?embed"
+          allowFullScreen
+          allow="fullscreen"
+        ></iframe>
+      </div>
+      <a
+        href="https://www.canva.com/design/DAGeJvmyQXo/sQA9RwG84mUQQZ7w2yYVcQ/view?utm_content=DAGeJvmyQXo&utm_campaign=designshare&utm_medium=embeds&utm_source=link"
+        target="_blank"
+        rel="noopener"
+      >
+        DCW February Newsletter 2025
+      </a>{' '}
+      by DreamCatchers WW
+    </>
+  )
+}
