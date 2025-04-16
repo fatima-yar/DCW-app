1ebf5d872bebf59f0839458c48bfd2ca4839cdfe
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-04-16 23:44:50 +1200

    add nz and Uk flag

--- frontend/components/Toggle.jsx
+++ frontend/components/Toggle.jsx
@@ -2,29 +2,22 @@
 import { useState } from 'react'
 
 export default function ToggleSwitch() {
-  const [isUK, setIsUK] = useState(true)
+  const [isUK, setIsUK] = useState(false)
 
   const toggleSwitch = () => {
-    const newValue = !isUK
-    setIsUK(newValue)
-
-    if (newValue) {
-      // window.location.href = '/'
-    } else {
-      // window.location.href = '/'
-    }
+    setIsUK(!isUK)
   }
 
   return (
-    <div className="flex items-center space-x-4 relative  lg:mx-50 md:mx-30 sm:mx-10  text-black overflow-hidden pb-4">
-      {/* NZ Flag - Show only when isUK is false */}
-      {!isUK && (
-        <img
-          src="/nzFlag.svg"
-          alt="New Zealand Flag"
-          className="w-6 h-6 md:w-8 md:h-8"
-        />
-      )}
+    <div className="flex items-center mx-54 gap-4 pb-4 text-black">
+      {/* NZ Flag */}
+      <img
+        src="/ukFlag.svg"
+        alt="UK Flag"
+        className={`w-10 h-10 md:w-8 md:h-8 transition-opacity duration-300 ${
+          isUK ? 'opacity-0' : 'opacity-100'
+        }`}
+      />
 
       {/* Toggle Button */}
       <div
@@ -37,16 +30,25 @@ export default function ToggleSwitch() {
             isUK ? 'right-0 text-right pr-2' : 'left-0 text-left pl-2'
           }`}
         >
-          {isUK ? 'UK' : 'NZ'}
+          {isUK ? 'NZ' : 'UK'}
         </span>
 
         {/* Circle */}
         <div
-          className={`w-4 h-4 md:w-6 md:h-6 border-2 text-black bg-[#8A5082] rounded-full shadow-md transform transition-transform duration-300 ${
-            isUK ? 'translate-x-0' : 'translate-x-9 md:translate-x-11 '
+          className={`w-6 h-6 bg-[#8A5082] text-black border-2 rounded-full shadow-md transform transition-transform duration-300 ${
+            isUK ? 'translate-x-0' : 'translate-x-11'
           }`}
         />
       </div>
+
+      {/* UK Flag */}
+      <img
+        src="/nzFlag.svg"
+        alt="New Zealand Flag"
+        className={`w-10 h-10 md:w-8 md:h-8 transition-opacity duration-300 ${
+          isUK ? 'opacity-100' : 'opacity-0'
+        }`}
+      />
     </div>
   )
 }
