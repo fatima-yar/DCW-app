84fe40ee1004d837454a32180f6e9a0828851a4c
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-04-17 12:26:00 +1200

    make navbarButton reusable and the fonts bigger

--- frontend/components/NavbarButtons.jsx
+++ frontend/components/NavbarButtons.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
+'use client'
 
-export default function NavbarWhatWeDo({ isOpen, handleClick }) {
+export default function NavbarButton({ isOpen, handleClick, label, subLabel }) {
   return (
     <div className="text-white font-[Convergence] hover:text-[#FFB635] hover:scale-105 hover:translate-x-1 transition-transform duration-200">
       <button onClick={handleClick} className="flex flex-col items-center">
         <div className="flex items-center justify-center">
-          <span className="text-md">What&nbsp;We&nbsp;Do</span>
+          <span className="text-lg">{label}</span>
           <span className="ml-2">{isOpen ? 'v' : '^'}</span>
         </div>
-        <span className="text-xs w-full text-center">
-          He&nbsp;aha&nbsp;ta&nbsp;tatou&nbsp;mahi
-        </span>
+        <span className="text-sm w-full text-center">{subLabel}</span>
       </button>
     </div>
   )
