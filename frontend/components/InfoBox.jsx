25e13a789940a2820fd702a77482471670148bdb
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-04-23 16:42:56 +1200

    do the first feedbacks

--- frontend/components/InfoBox.jsx
+++ frontend/components/InfoBox.jsx
@@ -6,7 +6,7 @@ export default function InfoBox() {
     <>
       <div className="pl-4 lg:pl-0 relative lg:mx-50 md:mx-30 sm:mx-10 py-20 text-black bg-white">
         <AboutUs />
-        <div className="pt-20">
+        <div className="pt-20 ">
           <WhatWeDo />
         </div>
       </div>
