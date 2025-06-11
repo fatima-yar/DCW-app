d971b3851d6eca03cf429d0672d7d350147b2b16
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-06-11 17:02:22 +1200

    ad

--- frontend/app/src/utils/get-strapi-url.ts
+++ frontend/app/src/utils/get-strapi-url.ts
@@ -1,3 +1,6 @@
 export default function getStrapiURL() {
-  return process.env.STRAPI_API_URL ?? 'http://localhost:1337'
+  return (
+    process.env.STRAPI_API_URL ??
+    'https://ingenious-song-020dc68f16.strapiapp.com'
+  )
 }
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,28 @@
+// import type { NextConfig } from 'next'
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'ingenious-song-020dc68f16.strapiapp.com',
+//         port: '1337',
+//         pathname: '/**',
+//       },
+//     ],
+//   },
+// }
+
+// export default nextConfig
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
+        protocol: 'https', // ✅ correct protocol
+        hostname: 'ingenious-song-020dc68f16.strapiapp.com',
         pathname: '/**',
       },
     ],
