7f9c2caad8250f256556b05a6a2e86958283b337
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-06-13 11:32:27 +1200

    add export in next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,24 +1,9 @@
-// import type { NextConfig } from 'next'
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'http',
-//         hostname: 'ingenious-song-020dc68f16.strapiapp.com',
-//         port: '1337',
-//         pathname: '/**',
-//       },
-//     ],
-//   },
-// }
-
-// export default nextConfig
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
