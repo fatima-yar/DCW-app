d415e8dde5707a4ed8a2ae6ea3192829127fb8bb
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-06-13 11:23:34 +1200

    add gitignore

--- frontend/app/src/utils/get-strapi-url.ts
+++ frontend/app/src/utils/get-strapi-url.ts
@@ -1,6 +1,6 @@
 export default function getStrapiURL() {
   return (
-    process.env.STRAPI_API_URL ??
+    process.env.NEXT_PUBLIC_API_URL ??
     'https://ingenious-song-020dc68f16.strapiapp.com'
   )
 }
