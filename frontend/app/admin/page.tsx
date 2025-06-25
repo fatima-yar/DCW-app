a7be4969dc6e432e0004f8bda645f38b258b810e
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2025-06-25 14:07:34 +1200

    add admin page

---
+++ frontend/app/admin/page.tsx
@@ -0,0 +1,15 @@
+export default function Admin() {
+  return (
+    <p className="text-black p-4 text-xl text-center">
+      Please click{' '}
+      <a
+        href="https://respected-example-0f150a69b7.strapiapp.com/admin"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <strong>here</strong>
+      </a>{' '}
+      to access the admin panel.
+    </p>
+  )
+}
